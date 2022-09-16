d5a0b93c437f048b87ea138cf87a1a6272c47b50
Simon - fantomitechno <fantomitechno@gmail.com>
2022-09-16 11:39:05 +0200

    chore: import deps *smart*

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"fmt"
+	"time"
+
+	"github.com/briandowns/spinner"
+	"github.com/fatih/color"
+	"github.com/manifoldco/promptui"
 )
 func main() {
 	fmt.Println("Hello, welcome to the calendar CLI")
