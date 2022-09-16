fed2c2b934ff1dadc281bec5ac5358861b7713c2
Simon - fantomitechno <fantomitechno@gmail.com>
2022-09-16 19:52:15 +0200

    chore: change build settings

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # Build for Linux (my default system)
-go build -o calendar-linux
+go build -o build/calendar-linux
 # Build for Windows
-GOOS=windows GOARCH=amd64 go build -o calendar-windows.exe
+GOOS=windows GOARCH=amd64 go build -o build/calendar-windows.exe
 # Build for Mac
-GOOS=darwin GOARCH=amd64 go build -o calendar-mac
+GOOS=darwin GOARCH=amd64 go build -o build/calendar-mac
