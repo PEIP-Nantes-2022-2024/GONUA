9d7ff1a8acf7e8e512553f4c610c1bcdbc137315
Simon - fantomitechno <fantomitechno@gmail.com>
2022-10-04 11:09:41 +0200

    fix: add beautify

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 # Build for Linux (my default system)
+echo "Building for Linux..."
 go build -o build/calendar-linux
+echo "Built for Linux"
 # Build for Windows
+echo "Building for Windows..."
 GOOS=windows GOARCH=amd64 go build -o build/calendar-windows.exe
+echo "Built for Windows"
 # Build for Mac
+echo "Building for Mac..."
 GOOS=darwin GOARCH=amd64 go build -o build/calendar-mac
+echo "Built for Mac"
